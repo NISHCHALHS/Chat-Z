85dd616e3300590c7740689820131e2a5619caec
Nishchal HS <nischalhs0l@gmail.com>
2026-03-16 23:15:18 +0530

    first commit

--- server.js
+++ server.js
@@ -1,55 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
-const User = require("./models/User");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/chatsz")
-.then(()=>console.log("Database connected"))
-.catch(err=>console.log(err));
+app.use("/auth", authRoutes);
 
-app.post("/signup", async (req,res)=>{
+mongoose.connect(" mongodb+srv://NISHCHALHS:<Nishchalhs9844>@cluster0.rv4syvw.mongodb.net/?appName=Cluster0")
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
 
-try{
-
-const {username,email,password} = req.body;
-
-const newUser = new User({
-username,
-email,
-password
-});
-
-await newUser.save();
-
-res.json({message:"Signup successful"});
-
-}catch(err){
-
-res.json({message:"User already exists"});
-
-}
-
-});
-
-app.post("/login", async (req,res)=>{
-
-const {email,password} = req.body;
-
-const user = await User.findOne({email,password});
-
-if(user){
-res.json({message:"Login successful"});
-}else{
-res.json({message:"Invalid email or password"});
-}
-
-});
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
 console.log("Server running on port 3000");
 });
